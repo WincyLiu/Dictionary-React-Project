b9eb3729d8b786c4b3bf328950fbe35085d4f02b
WincyLiu <87019971+WincyLiu@users.noreply.github.com>
2021-11-19 15:12:10 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <main>
+            <Dictionary />
+          </main>
+        </header>
+        <footer className="App-footer">Coded by Wincy Liu</footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
