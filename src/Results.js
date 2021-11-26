e6b95500a4d794d7731a071310e0118cdfec459c
WincyLiu <87019971+WincyLiu@users.noreply.github.com>
2021-11-26 17:29:47 +0000

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -6,10 +6,11 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
-        {props.results.meanings.map(function (meaning, index) {
-          return (
-            <div key={index}>
+        <section>
+          <h2>{props.results.word}</h2>
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
                 <Phonetic phonetic={phonetic} />
               </div>
             );
