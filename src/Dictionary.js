3c32807cc5a36f67ffd39c04690c3e7cf38776b4
WincyLiu <87019971+WincyLiu@users.noreply.github.com>
2021-11-26 17:28:15 +0000

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,12 +7,13 @@ import Photos from "./Photos";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
-  
-   function handlePexelsResponse(response) {
+
+  function handlePexelsResponse(response) {
     setPhotos(response.data.photos);
   }
 
@@ -20,14 +21,13 @@ export default function Dictionary() {
     event.preventDefault();
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
-    
-     let pexelsApiKey =
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
       "563492ad6f917000010000019a5c44eba558428ca26c00e3e336d8cc";
     let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
-  }
   }
 
   function handleKeywordChange(event) {
@@ -36,15 +36,18 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
-    <h1 className="text-center mt-3 mb-3"> Dictionary</h1>
-      <form onSubmit={search}>
-        <input  className="form-control"
-          type="search"
-          autoFocus={true}
-          onChange={handleKeywordChange}
-          placeholder="Search for a word"
-        />
-      </form>
+      <h1 className="text-center mt-3 mb-3"> Dictionary</h1>
+      <section>
+        <form onSubmit={search}>
+          <input
+            className="form-control"
+            type="search"
+            autoFocus={true}
+            onChange={handleKeywordChange}
+            placeholder="Search for a word"
+          />
+        </form>
+      </section>
       <Results results={results} />
       <Photos photos={photos} />
     </div>
