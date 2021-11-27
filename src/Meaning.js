8afc253f379c11115e81411a98079214645ba1a6
WincyLiu <87019971+WincyLiu@users.noreply.github.com>
2021-11-27 17:52:20 +0000

    Linked to css

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   return (
@@ -7,15 +8,10 @@ export default function Meaning(props) {
       <h3>{props.meaning.partOfSpeech} </h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
-          <div key={index}>
-            <p>
-              <strong>Definition: </strong>
-              {definition.definition}
-              <br />
-              <strong>Example:</strong> <em>{definition.example} </em>
-              <br />
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
+           <div key={index}>
+            <div className="defintion">{definition.definition}</div>
+            <div className="example">{definition.example} </div>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
